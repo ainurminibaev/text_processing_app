eea8422717873a94d11a6c160bb8d5571df18afe
Ainur Minibaev <rapper01@list.ru>
2015-05-21 13:47:51 +0300

    dirty hsql server

--- src/main/java/pack/config/DataSourceConfig.java
+++ src/main/java/pack/config/DataSourceConfig.java
@@ -1,40 +1,42 @@
 package pack.config;
 
 import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import pack.db.HyperSqlDbServer;
 
 import javax.sql.DataSource;
 import java.beans.PropertyVetoException;
 import java.net.URISyntaxException;
+import java.util.Properties;
 
 /**
  * Created on 10.09.2014.
  */
 @Configuration
 @PropertySource(value = "classpath:database.properties", ignoreResourceNotFound = true)
-public class DataSourceConfig {
+public class DataSourceConfig implements DisposableBean {
 
     @Autowired
     private Environment env;
 
+    private HyperSqlDbServer hyperSqlDbServer;
+
     @Bean
     public DataSource dataSource() throws PropertyVetoException, URISyntaxException {
         String username, password, dbUrl, driverClassName;
+        String mode = env.getProperty("jdbc.mode");
+        if (mode.contains("hsql")) {
+            createHsqlDataSource();
+        }
         username = env.getProperty("jdbc.user");
         password = env.getProperty("jdbc.password");
         dbUrl = env.getProperty("jdbc.url");
         driverClassName = env.getProperty("jdbc.driver");
-        if (driverClassName.toLowerCase().contains("hsql")) {
-            return new EmbeddedDatabaseBuilder()
-                    .setType(EmbeddedDatabaseType.HSQL)
-                    .build();
-        }
         ComboPooledDataSource dataSource = new ComboPooledDataSource();
         dataSource.setDriverClass(driverClassName);
         dataSource.setJdbcUrl(dbUrl);
@@ -44,4 +46,17 @@ public class DataSourceConfig {
         return dataSource;
     }
 
+    private DataSource createHsqlDataSource() {
+        hyperSqlDbServer = new HyperSqlDbServer();
+        hyperSqlDbServer.start();
+        return null;
+    }
+
+
+    @Override
+    public void destroy() throws Exception {
+        if (hyperSqlDbServer != null) {
+            hyperSqlDbServer.stop();
+        }
+    }
 }
